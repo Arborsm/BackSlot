2ba6c1f4ed1358be9e95969beb22d85bd12aa009
Globox1997 <mitschke11@web.de>
2024-05-10 18:25:39 +0200

    other way

--- src/main/java/net/backslot/network/VisibilityPacket.java
+++ src/main/java/net/backslot/network/VisibilityPacket.java
@@ -10,14 +10,29 @@ public record VisibilityPacket(int entityId, int slotId, ItemStack itemStack) im
 
     public static final CustomPayload.Id<VisibilityPacket> PACKET_ID = new CustomPayload.Id<>(new Identifier("backslot", "visibility_packet"));
 
-    public static final PacketCodec<RegistryByteBuf, VisibilityPacket> PACKET_CODEC = PacketCodec.of((value, buf) -> {
-        buf.writeInt(value.entityId);
-        buf.writeInt(value.slotId);
-        buf.writeBoolean(value.itemStack.isEmpty());
-        if (!value.itemStack.isEmpty()) {
-            ItemStack.PACKET_CODEC.encode(buf, value.itemStack);
+    // public static final PacketCodec<RegistryByteBuf, VisibilityPacket> PACKET_CODEC = PacketCodec.of((value, buf) -> {
+    // buf.writeInt(value.entityId);
+    // buf.writeInt(value.slotId);
+    // buf.writeBoolean(value.itemStack.isEmpty());
+    // if (!value.itemStack.isEmpty()) {
+    // ItemStack.PACKET_CODEC.encode(buf, value.itemStack);
+    // }
+    // }, buf -> new VisibilityPacket(buf.readInt(), buf.readInt(), buf.readBoolean() ? ItemStack.EMPTY : ItemStack.PACKET_CODEC.decode(buf)));
+
+    public static final PacketCodec<RegistryByteBuf, VisibilityPacket> PACKET_CODEC = PacketCodec.of(VisibilityPacket::write, VisibilityPacket::new);
+
+    public VisibilityPacket(RegistryByteBuf buf) {
+        this(buf.readInt(), buf.readInt(), buf.readBoolean() ? ItemStack.EMPTY : ItemStack.PACKET_CODEC.decode(buf));
+    }
+
+    public void write(RegistryByteBuf buf) {
+        buf.writeInt(entityId);
+        buf.writeInt(slotId);
+        buf.writeBoolean(itemStack.isEmpty());
+        if (!itemStack.isEmpty()) {
+            ItemStack.PACKET_CODEC.encode(buf, itemStack);
         }
-    }, buf -> new VisibilityPacket(buf.readInt(), buf.readInt(), buf.readBoolean() ? ItemStack.EMPTY : ItemStack.PACKET_CODEC.decode(buf)));
+    }
 
     @Override
     public Id<? extends CustomPayload> getId() {
